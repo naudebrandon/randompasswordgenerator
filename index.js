7e897e7e104ca444393da9ad4faa5b98614ae391
Brandon Naude <naudebrandon@gmail.com>
2022-10-10 21:07:09 +0200

    added the functions for generate button
    added the functions for generate button, and copy to clipboard

--- index.js
+++ index.js
@@ -2,3 +2,44 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 "l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=",
 "{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+//Get the 2 display fields
+let passwordOne = document.getElementById("password-one")
+let passwordTwo = document.getElementById("password-two")
+let password="";
+
+function randomGenerator(){
+    let randomCharacter = Math.floor(Math.random() * characters.length)
+    return characters[randomCharacter]
+}
+
+function generatePasswords(){
+    passOne="";
+    passTwo="";
+
+    for (let i = 0; i < 16; i++)
+    {
+        password = randomGenerator();
+        passOne += password;
+        password = randomGenerator();
+        passTwo += password;
+    }
+    
+    //Display generated passwords
+    passwordOne.textContent = passOne;
+    passwordTwo.textContent = passTwo;
+}
+
+//Function to copy generated password to the clipboard
+function copyToClipboardP1() {
+    var copyText = passOne;
+    navigator.clipboard.writeText(copyText).then(() => {
+        alert("Copied to clipboard");
+    });
+  }
+
+  function copyToClipboardP2() {
+    var copyText = passTwo;
+    navigator.clipboard.writeText(copyText).then(() => {
+        alert("Copied to clipboard");
+    });
+  }
